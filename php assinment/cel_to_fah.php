4986876a772156997cccdfc421b917b765e71cf3
Shahriar Rabby <and.rabby5@gmail.com>
2024-07-11 12:55:56 +0600

    Update cel_to_fah.php

--- php assinment/cel_to_fah.php
+++ php assinment/cel_to_fah.php
@@ -4,7 +4,7 @@
 
     $temp_in_cel = 32;
     $temp_in_fah = (($temp_in_cel*9)/5+32);
-    printf("The converted temparature of ".$temp_in_cel." degree C to F is:". number_format($temp_in_fah,5));
+    printf("The converted temparature of ".$temp_in_cel." degree C to F is:". number_format($temp_in_fah,2));
 
 
 
